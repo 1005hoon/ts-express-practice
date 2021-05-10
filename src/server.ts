d6560136c2a7b8c8995b6859d8945100b4de3ea7
Hoon <shoh@help-me.kr>
2021-05-10 10:39:46 +0900

    conenct to postgres and create config for typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 // import env vars
 import "dotenv/config";
-import "./utils/validateEnv";
 
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import App from "./app";
+import config from "./ormconfig";
 
 import PostController from "./posts/posts.controller";
-import AuthController from "./auth/auth.controllers";
 
-const app = new App([new PostController(), new AuthController()]);
-
-app.listen();
+(async () => {
+  try {
+    await createConnection(config);
+    console.log(`Server connected to DB: ${config.database}`);
+  } catch (error) {
+    console.log(`Error while connecting to DB: ${error}`);
+    return error;
+  }
+  const app = new App([new PostController()]);
+  app.listen();
+})();
